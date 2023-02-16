4ca7f90631c625cb9a3aa9c9061b59228a8e1968
Doni Nurramdan <doni.nurramdan@indocyber.id>
2023-02-16 11:48:59 +0700

    add function send email to client brithday

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const promise_1 = __importDefault(require("mysql2/promise"));
-const fetchUsers = ({ email, verifiedStatus, isBirthday }) => __awaiter(void 0, void 0, void 0, function* () {
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const fetchUsers = (params) => __awaiter(void 0, void 0, void 0, function* () {
     // Connect to the database
     const connection = yield promise_1.default.createConnection({
         host: 'localhost',
@@ -23,26 +24,52 @@ const fetchUsers = ({ email, verifiedStatus, isBirthday }) => __awaiter(void 0, 
     });
     // Build the query
     let query = 'SELECT * FROM users a join profile b on a.idUser=b.idUser WHERE a.email = ?';
-    const params = [email];
-    if (verifiedStatus !== undefined) {
+    const paramsQuery = [params.email];
+    if (params.verifiedStatus !== undefined) {
         query += ' AND a.status = ?';
-        params.push(verifiedStatus);
+        paramsQuery.push(params.verifiedStatus);
     }
-    if (isBirthday) {
+    if (params.isBirthday) {
         const today = new Date();
         const month = today.getMonth() + 1;
         const day = today.getDate();
         query += ` AND MONTH(b.tanggalLahir) = ${month} AND DAY(b.tanggalLahir) = ${day}`;
     }
     // Execute the query and return the results
-    console.log("ini query", query);
-    console.log("ini params", params);
-    const [rows] = yield connection.execute(query, params);
+    const [rows] = yield connection.execute(query, paramsQuery);
     return rows;
+});
+const sendNotification = (params) => __awaiter(void 0, void 0, void 0, function* () {
+    const transporter = nodemailer_1.default.createTransport({
+        service: "gmail",
+        auth: {
+            user: "nurramdandoni@gmail.com",
+            pass: "bzjbpzstogabfjfp",
+        },
+    });
+    const mailOptions = {
+        from: "nurramdandoni@gmail.com",
+        to: params.to,
+        subject: params.subject,
+        text: params.text,
+    };
+    try {
+        const info = yield transporter.sendMail(mailOptions);
+        console.log("Email sent: " + info.response);
+    }
+    catch (err) {
+        console.error("Failed to send email: ", err);
+    }
 });
 fetchUsers({ email: "nurramdandoni@gmail.com", verifiedStatus: "Active", isBirthday: true })
     .then((users) => {
     console.log(users);
+    const params = {
+        to: "nurramdandoni@gmail.com",
+        subject: "Test Email Suco",
+        text: "This is a test email Suco",
+    };
+    sendNotification(params);
 }).catch((err) => {
     console.log(err);
 });
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import  mysql  from 'mysql2/promise';
-
+import nodemailer from "nodemailer";
 interface UserFilterField {
   email: string;
   verifiedStatus?: string;
@@ -14,7 +14,13 @@ interface User {
   birthday?: Date;
 }
 
-const fetchUsers = async ({ email, verifiedStatus, isBirthday }: UserFilterField): Promise<User[]> => {
+interface NotificationParams {
+    to: string;
+    subject: string;
+    text: string;
+  }
+
+const fetchUsers = async (params: UserFilterField): Promise<User[]> => {
   // Connect to the database
   const connection = await mysql.createConnection({
     host: 'localhost',
@@ -25,14 +31,14 @@ const fetchUsers = async ({ email, verifiedStatus, isBirthday }: UserFilterField
 
   // Build the query
   let query = 'SELECT * FROM users a join profile b on a.idUser=b.idUser WHERE a.email = ?';
-  const params = [email];
+  const paramsQuery = [params.email];
 
-  if (verifiedStatus !== undefined) {
+  if (params.verifiedStatus !== undefined) {
     query += ' AND a.status = ?';
-    params.push(verifiedStatus);
+    paramsQuery.push(params.verifiedStatus);
   }
 
-  if (isBirthday) {
+  if (params.isBirthday) {
     const today = new Date();
     const month = today.getMonth() + 1;
     const day = today.getDate();
@@ -40,15 +46,45 @@ const fetchUsers = async ({ email, verifiedStatus, isBirthday }: UserFilterField
   }
 
   // Execute the query and return the results
-  const [rows] = await connection.execute(query, params);
+  const [rows] = await connection.execute(query, paramsQuery);
   return rows as User[];
 
 };
+
+const sendNotification = async (params: NotificationParams) => {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "nurramdandoni@gmail.com", // isi dengan email yang diinginkan
+        pass: "bzjbpzstogabfjfp", // ubah dengan sandi sementara anda
+      },
+    });
+  
+    const mailOptions = {
+      from: "nurramdandoni@gmail.com",
+      to: params.to,
+      subject: params.subject,
+      text: params.text,
+    };
+  
+    try {
+      const info = await transporter.sendMail(mailOptions);
+      console.log("Email sent: " + info.response);
+    } catch (err) {
+      console.error("Failed to send email: ", err);
+    }
+  }
 
 fetchUsers({email:"nurramdandoni@gmail.com",verifiedStatus:"Active",isBirthday:true})
 .then(
   (users) => {
     console.log(users);
+    const params: NotificationParams = {
+        to: "nurramdandoni@gmail.com",
+        subject: "Test Email Suco",
+        text: "This is a test email Suco",
+      };
+    sendNotification(params)
   }
 ).catch(
   (err) => {
